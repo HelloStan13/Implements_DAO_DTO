978539c5ed6484c51a75621e20e66e45686f94cd
Adryan <adryanynfante@gmail.com>
2022-07-30 13:17:09 -0500

    se crea proyecto

---
+++ src/main/java/DTO/DTODemo.java
@@ -0,0 +1,30 @@
+package DTO;
+
+import BO.ClienteBO;
+import VO.ClienteVO;
+
+public class DTODemo {
+    public static void main(String[] args) {
+        //objeto business object
+        ClienteBO clienteBusinessObject = new ClienteBO();
+
+        //obtiene todos los clientes
+        clienteBusinessObject.obtenerClientes().forEach(System.out::println);
+
+        // actualiza un cliente
+        System.out.println("****");
+        ClienteVO cliente = clienteBusinessObject.obtenerCliente(0);
+        cliente.setNombre("Adryan");
+        clienteBusinessObject.actualizarCliente(cliente);
+
+        // obtiene un cliente
+        System.out.println("****");
+        cliente=clienteBusinessObject.obtenerCliente(0);
+        System.out.println(cliente);
+
+        //elimina un cliente
+        System.out.println("****");
+        cliente=clienteBusinessObject.obtenerCliente(0);
+        clienteBusinessObject.eliminarCliente(cliente);
+    }
+}
