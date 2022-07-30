ec5e3665b41a532173dc0b88d46e1822918d59a2
Adryan <adryanynfante@gmail.com>
2022-07-30 12:26:57 -0500

    inicio proyecto
    se crea clase employee para luego implementar DAO y DTO

---
+++ src/main/java/Model/Employee.java
@@ -0,0 +1,44 @@
+package Model;
+
+public class Employee {
+   private String name;
+    private String apellido;
+    private String direccion;
+    private int edad;
+
+
+ public Employee() { }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
